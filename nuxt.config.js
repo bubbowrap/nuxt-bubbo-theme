e1bed61b417e16499ce78eb18993efe293c625d4
Jeremy Samuel <pentium6.0@gmail.com>
2021-01-30 12:19:36 -0500

    loading fonts quicker

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,9 +22,11 @@ export default {
     ],
 
     link: [
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: 'stylesheet',
+        rel: ['stylesheet preload'],
+        as: 'style',
         href:
           'https://fonts.googleapis.com/css?family=Palanquin:300,400,700&display=swap',
       },
