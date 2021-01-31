2257b4019ab1bfa1d8776c46dfe700b41314371e
Jeremy Samuel <pentium6.0@gmail.com>
2021-01-31 16:11:06 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,8 +36,7 @@ export default {
 
     link: [
       { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'canonical', href: 'https://jeremysamuel.dev' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }    
     ],
 
     script: [
